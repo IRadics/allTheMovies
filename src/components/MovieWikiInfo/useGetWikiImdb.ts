19de2e434bec4e97a64e502e47cc30d73ebf62e5
IRadics <radics_istvan@hotmail.com>
2022-10-17 19:24:13 +0200

    Added function to get wiki match and imdb link

---
+++ src/components/MovieWikiInfo/useGetWikiImdb.ts
@@ -0,0 +1,77 @@
+import { useWikiGetExtLinks } from "../../REST/wikipedia/useWikiGetExtLinks";
+import { useWikiSearch } from "../../REST/wikipedia/useWikiSearch";
+import { useState, useEffect } from "react";
+import { stringSimilarity } from "string-similarity-js";
+
+type WikiPageCandidate = {
+  pageId: number;
+  title: string;
+  imdbLink?: string;
+  releaseDateFound?: boolean;
+  probability?: number;
+};
+
+export const useGetWikiImdb = (searchTerm: string, releaseYear: number) => {
+  const [candidates, setCandidates] = useState<WikiPageCandidate[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [wikiPageId, setWikiPageId] = useState<number>();
+  const [imdbLink, setImdbLink] = useState<string>("");
+
+  const wikiSearch = useWikiSearch(searchTerm, 30);
+  const extLinksQuery = useWikiGetExtLinks(candidates.map((c) => c.pageId));
+
+  const findReleaseYear = (pageId: number) => {
+    if (!releaseYear) return false;
+    return wikiSearch.data?.query.search
+      .find((s) => s.pageid === pageId)
+      ?.snippet.includes(releaseYear.toString());
+  };
+
+  useEffect(() => {
+    if (!wikiSearch?.loading && wikiSearch?.data) {
+      let pageInfo = wikiSearch!.data?.query.search.map((id) => {
+        return { pageId: id.pageid, title: id.title };
+      });
+      setCandidates(candidates.concat(pageInfo));
+    }
+  }, [wikiSearch?.loading]);
+
+  useEffect(() => {
+    if (!extLinksQuery.loading && extLinksQuery.data) {
+      candidates.forEach((cand) => {
+        cand.imdbLink = extLinksQuery.data?.query.pages
+          .find((p) => p.pageid === cand.pageId)
+          ?.extlinks?.map((l) => l.url)
+          .find((e) => e.includes("imdb.com/title/"));
+        cand.releaseDateFound = findReleaseYear(cand.pageId);
+        cand.probability =
+          ((cand.imdbLink ? 1 : 0) +
+            (cand.releaseDateFound ? 1 : 0) +
+            Math.max(
+              stringSimilarity(cand.title, searchTerm),
+              stringSimilarity(cand.title + " (film)", searchTerm),
+              stringSimilarity(cand.title + " (movie)", searchTerm),
+              stringSimilarity(cand.title + ` ${releaseYear}`, searchTerm)
+            ) *
+              1.5) /
+          3.5;
+      });
+
+      let sorted = candidates
+        .sort((x, b) => {
+          return x.probability! - b.probability!;
+        })
+        .reverse();
+      if (candidates[0].probability! > 0.65) {
+        if (candidates[0].imdbLink) setImdbLink(candidates[0].imdbLink);
+
+        setWikiPageId(candidates[0].pageId);
+      } else {
+        setImdbLink("");
+        setWikiPageId(undefined);
+      }
+      setLoading(false);
+    }
+  }, [extLinksQuery.loading]);
+  return { loading, wikiPageId, imdbLink };
+};
