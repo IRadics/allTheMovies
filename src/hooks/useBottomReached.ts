a0b2707c9581fe64950709ed43b13203a20fa5fb
IRadics <radics_istvan@hotmail.com>
2022-10-27 09:26:39 +0200

    Added a hook to detect if the user scrolled to the bottom of the page

---
+++ src/hooks/useBottomReached.ts
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from "react";
+/**
+ * Triggers when the user scrolled to the bottom of the HTML element
+ *
+ * @param element - the scrollable HTML element
+ * @param callback  - called when scrolled to the bottom of the element - WITH delay if provided)
+ * @param delay     -[optional] delay in ms.
+ * @returns boolean -  true if the user scrolled to the bottom of the element - WITHOUT delay so it can trigger even if the callback does not
+ */
+export function useBottomReached(
+  element: HTMLElement,
+  callback?: (e: Event) => void,
+  delay?: number
+) {
+  const [isOnBottom, setIsOnBottom] = useState<boolean>(false);
+
+  let timeoutHandler = useRef<NodeJS.Timeout | undefined>(undefined);
+
+  const setState = useRef<Function>((state: boolean) => {
+    setIsOnBottom(state);
+  });
+
+  useEffect(() => {
+    if (element) {
+      element.addEventListener("scroll", trackScrolling);
+      return () => {
+        element.removeEventListener("scroll", trackScrolling);
+      };
+    }
+    // eslint-disable-next-line
+  }, [element]);
+
+  const isBottom = (element: HTMLElement) => {
+    return element.scrollHeight - element.scrollTop <= element.clientHeight;
+  };
+
+  const trackScrolling = (e: Event) => {
+    if (delay) {
+      if (isBottom(element)) {
+        setState.current(true);
+
+        if (timeoutHandler.current) {
+          clearTimeout(timeoutHandler.current);
+        }
+        timeoutHandler.current = setTimeout(() => {
+          if (callback) callback(e);
+        }, delay);
+      } else {
+        setState.current(false);
+
+        if (timeoutHandler.current !== undefined) {
+          clearTimeout(timeoutHandler.current);
+          timeoutHandler.current = undefined;
+        }
+      }
+    } else {
+      if (isBottom(element)) {
+        setState.current(true);
+        if (callback) callback(e);
+      } else {
+        setState.current(false);
+      }
+    }
+  };
+
+  return isOnBottom;
+}
